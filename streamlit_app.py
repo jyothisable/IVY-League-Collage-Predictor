739866d104f4f11d61c3b7296212a7e585f512d9
jyothisable <athuljyothis18@gmail.com>
2024-06-20 04:28:09 +0530

    removed feature dep

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import joblib
 
-from build_features import * # import all functions in build_features.py
+# from build_features import * # import all functions in build_features.py
 # #column transformer
 
 lr_model = joblib.load('models/LR_model.joblib')
